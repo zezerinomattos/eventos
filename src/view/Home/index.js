3cf320ba975e154ab6f3e48a5be55c006a353cd1
zezerinodev <mattoszz@hotmail.com>
2022-08-23 17:22:09 -0300

    Pagina Home inicializada

---
+++ src/view/Home/index.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import './style.css';
+import { Link } from 'react-router-dom';
+
+
+function Home(){
+    return(
+        <h1>Pagina Home</h1>
+    );
+}
+
+export default Home;
