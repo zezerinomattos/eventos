5834e8fef5348806f8cf9da20628fff28c4a9afb
zezerinodev <mattoszz@hotmail.com>
2022-10-05 17:14:51 -0300

    Armazenando o estado de login do usuário no navegador

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from '../store/';
+import { store, persistor } from '../store/';
 
 import Login from '../view/login';
 import NovoUsuario from '../view/usuario-novo';
@@ -14,18 +15,20 @@ import EventoDetalhes from "../view/evento-detalhes";
 export default function Rotas(){
     return(
         <Provider store={store}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/eventos/:parametro" element={<Home />} />
-                    <Route path="/novousuario" element={<NovoUsuario />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/usuariorecuperarsenha" element={<UsuarioRecuperarSenha />} />
-                    <Route path="/eventoscadastro" element={<EventosCadastro />} />
-                    <Route path="/eventodetalhes/:id" element={<EventoDetalhes />} /> 
-                    <Route path="/editarevento/:id" element={<EventosCadastro />} /> 
-                </Routes>
-            </BrowserRouter>
+            <PersistGate loading={null} persistor={persistor} >
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/eventos/:parametro" element={<Home />} />
+                        <Route path="/novousuario" element={<NovoUsuario />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/usuariorecuperarsenha" element={<UsuarioRecuperarSenha />} />
+                        <Route path="/eventoscadastro" element={<EventosCadastro />} />
+                        <Route path="/eventodetalhes/:id" element={<EventoDetalhes />} /> 
+                        <Route path="/editarevento/:id" element={<EventosCadastro />} /> 
+                    </Routes>
+                </BrowserRouter>
+            </PersistGate>
         </Provider>
     );
 }
